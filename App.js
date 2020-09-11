534e3202f535cd3b4747594446d47499c7e3eda6
dkersten <dkersten1994@gmail.com>
2020-09-11 15:47:07 -0400

    built out landing page

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>EZ Golf Scorecard</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+        style={styles.bgGradient}
+        colors={['#02A637', '#00E952', '#02A637']}
+      >
+        <Text style={styles.headline}>EZ Golf Scorecard</Text>
+        <StatusBar style="auto" />
+      </LinearGradient>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#02A637',
+  },
+  bgGradient: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  headline: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '700',
   },
 });
